f8ddbb1438d8549d1fc9481b8d446896d766b7c7
ANDREI VISCAYNO <2018006164@dhvsu.edu.ph>
2024-10-11 06:13:16 +0800

    MAIN

--- dashboard/admin/controller/receive_data.php
+++ dashboard/admin/controller/receive_data.php
@@ -26,7 +26,7 @@ class SensorLogger
         ];
 
         // Sensors to exclude from logging (e.g., soil moisture sensors)
-        $this->excludedSensors = ['soilMoisture1', 'soilMoisture2', 'timestamp'];
+        $this->excludedSensors = ['soilMoisture1', 'soilMoisture2', 'timestamp', 'wifi_status'];
     }
 
     // Fetch data from proxy server
